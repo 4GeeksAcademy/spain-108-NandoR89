4628e9469b85d95565e816b0dda82fcc99cab97a
Fernando R. G. <nando.rodriguez.gomez89@gmail.com>
2025-06-06 18:49:31 +0000

    he añadido el modal con react-bootstrap

--- src/front/pages/AddNewContact.jsx
+++ src/front/pages/AddNewContact.jsx
@@ -1,57 +1,98 @@
 import { useState } from "react"
+import { Link } from "react-router-dom"
 
 
 export const AddNewContact = () => {
 
-    const [contactName, setContactName] = useState('')
-    const [contactPhoneNumber, setContactPhoneNumber] = useState('')
-    const [contactEmail, setContactEmail] = useState('')
-    const [contactAdress, setContactAdress] = useState('')
+  const userName = "NandoR89"
+  const host = 'https://playground.4geeks.com/contact'
+  const getUrl = `${host}/agendas/${userName}`
+  const postUrlUser = `${host}/agendas/${userName}`
+  const postUrl = `${host}/agendas/${userName}/contacts`
 
-    const handleFullName = event => setContactName(event.target.value)
-    const handleEmail = event => setContactEmail(event.target.value)
-    const handlePhone = event => setContactPhoneNumber(event.target.value)
-    const handleAddress = event => setContactAdress(event.target.value)
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        
-        setContactName('')
-        setContactPhoneNumber('')
-        setContactEmail('')
-        setContactAdress('')
+  const [contactName, setContactName] = useState('')
+  const [contactPhoneNumber, setContactPhoneNumber] = useState('')
+  const [contactEmail, setContactEmail] = useState('')
+  const [contactAdress, setContactAdress] = useState('')
+  const [contacts, setContacts] = useState([])
 
+  const handleFullName = event => setContactName(event.target.value)
+  const handleEmail = event => setContactEmail(event.target.value)
+  const handlePhone = event => setContactPhoneNumber(event.target.value)
+  const handleAddress = event => setContactAdress(event.target.value)
+
+  const getContacts = async () => {
+    try {
+      const response = await fetch(getUrl)
+      if (!response.ok && response.status === 404) {
+        addUser()
+        return
+      }
+      const data = await response.json()
+      setContacts(Array.isArray(data) ? data : data.contacts || [])
+    } catch (error) {
+      console.error("Error al cargar las tareas", error)
     }
+  }
 
-    return (
-        <div className="container m-auto">
-            <form onSubmit={handleSubmit}>
-                <div className="row">
-                    <div className="col-md-10 col-lg-8 col-xl-6">
-                        <label htmlFor="fullname" className="form-label">Full Name</label>
-                        <input onChange={handleFullName} id="fullname" type="text" className="form-control" value={contactName} placeholder="Enter full name" />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-10 col-lg-8 col-xl-6">
-                        <label htmlFor="" className="form-label">Email</label>
-                        <input onChange={handleEmail} type="email" className="form-control" value={contactEmail} placeholder="Enter email" />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-10 col-lg-8 col-xl-6">
-                        <label htmlFor="" className="form-label">Phone</label>
-                        <input onChange={handlePhone} type="tel" className="form-control" value={contactPhoneNumber} placeholder="Enter phone" />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-10 col-lg-8 col-xl-6">
-                        <label htmlFor="" className="form-label">Address</label>
-                        <input onChange={handleAddress} type="text" className="form-control" value={contactAdress} placeholder="Enter address" />
-                    </div>
-                </div>
-                <button type="submit" className="btn btn-primary col-12 col-md-10 col-lg-8 col-xl-6 mt-4">Save</button>
-            </form>
+  const addUser = async () => {
+    try {
+      const response = await fetch(postUrlUser, {
+        method: "POST",
+        body: JSON.stringify({}),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      if (response.ok) {
+        getContacts()
+      }
+    } catch (error) {
+      console.error("Error al crear el contacto", error)
+    }
+  }
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    setContactName('')
+    setContactPhoneNumber('')
+    setContactEmail('')
+    setContactAdress('')
+  }
+
+  return (
+    <div className="container m-auto">
+      <form onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="col-md-10 col-lg-8 col-xl-6">
+            <label htmlFor="fullname" className="form-label">Full Name</label>
+            <input onChange={handleFullName} id="fullname" type="text" className="form-control" value={contactName} placeholder="Enter full name" />
+          </div>
         </div>
-    )
+        <div className="row">
+          <div className="col-md-10 col-lg-8 col-xl-6">
+            <label htmlFor="" className="form-label">Email</label>
+            <input onChange={handleEmail} type="email" className="form-control" value={contactEmail} placeholder="Enter email" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-10 col-lg-8 col-xl-6">
+            <label htmlFor="" className="form-label">Phone</label>
+            <input onChange={handlePhone} type="tel" className="form-control" value={contactPhoneNumber} placeholder="Enter phone" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-10 col-lg-8 col-xl-6">
+            <label htmlFor="" className="form-label">Address</label>
+            <input onChange={handleAddress} type="text" className="form-control" value={contactAdress} placeholder="Enter address" />
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary col-12 col-md-10 col-lg-8 col-xl-6 mt-4">Save</button>
+      </form>
+      <Link to="/contact">or get back to contact</Link>
+    </div>
+  )
 }
--- src/front/pages/Contact.jsx
+++ src/front/pages/Contact.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
-export const Contact = () => {
+export const Contact = (item) => {
+
+  const defaultImage = "https://images.unsplash.com/photo-1748444406895-c284c355e2ef?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  const imageUrl = item?.image ? item.image : defaultImage
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleEdit = () => {
+  }
+
+  const handleDelete = () => {
+    setShowModal(true)
+  }
 
   return (
     <div className="container mt-3">
@@ -9,22 +26,47 @@ export const Contact = () => {
           Add New Contact
         </Link>
       </div>
-      <div className="card my-3">
+      <div key={item.id} className="card my-3">
         <div className="row g-0">
-          <div className="col-md-4">
-            <img src="..." className="img-fluid rounded-start" alt="..." />
+          <div className="col-md-4 text-center m-auto">
+            <img src={imageUrl} className="img-fluid rounded-circle w-75 h-75" alt="..." />
           </div>
-          <div className="col-md-8">
+          <div className="col-md-8 d-flex">
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+              <h5 className="card-title">Full Name</h5>
+              <p className="card-text">Address</p>
+              <p className="card-text">Phone</p>
+              <p className="card-text">Email</p>
             </div>
+            <div className="mt-3 me-3">
+              <i onClick={handleEdit} className="fa-solid fa-pen-to-square fa-xl me-3" style={{ cursor: "pointer" }}></i>
+              <i onClick={handleShow} className="fa-solid fa-trash fa-xl" style={{ cursor: "pointer" }}></i>
+            </div>
+            {show && (
+              <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+              >
+                <Modal.Header closeButton>
+                  <Modal.Title>Eliminar Contacto</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  Estás seguro de querer eliminar este contacto?
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Cerrar
+                  </Button>
+                  <Button onClick={handleDelete} variant="danger">Eliminar</Button>
+                </Modal.Footer>
+              </Modal>
+            )
+            }
           </div>
         </div>
       </div>
     </div>
-
-
   )
 }
