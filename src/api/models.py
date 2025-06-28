852494d53d9e3510b08448c41f28703cbc48d37c
Fernando R. G. <nando.rodriguez.gomez89@gmail.com>
2025-06-28 10:53:05 +0000

    añado todas las clases en la tabla

--- src/api/models.py
+++ src/api/models.py
@@ -9,14 +9,72 @@ class Users(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.String, nullable=True)
+    last_name = db.Column(db.String, nullable=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
 
     def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+        # Do not serialize the password, its a security breach
+        return {"id": self.id,
+                "email": self.email,
+                "is_active": self.is_active,
+                "first_name": self.first_name,
+                "last_name": self.last_name}
     
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+    body = db.Column(db.String)
+    date = db.Column(db.Date, nullable=False)
+    image_url = db.Column(db.String)
+
+
+class Media(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    media_type = db.Column(db.Enum('image', 'video', 'audio', name='media_type'), nullable=False)
+    image_url = db.Column(db.String)
+
+
+class Followers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+
+
+class CharacterFavorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class PlanetsFavorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    height = db.Column(db.String)
+    mass = db.Column(db.String)
+    hair_color = db.Column(db.String)
+    skin_color = db.Column(db.String)
+    eye_color = db.Column(db.String)
+    birth_year = db.Column(db.String)
+    gender = db.Column(db.String)
+
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    diameter = db.Column(db.String)
+    rotation_period = db.Column(db.String)
+    prbital_period = db.Column(db.String)
+    gravity = db.Column(db.String)
+    population = db.Column(db.String)
+    climate = db.Column(db.String)
+    terrain = db.Column(db.String)
